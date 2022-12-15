29fb771dca2ca0f45d3c492676e1a79a5c0d1fc9
SupervisedThinking <supervisedthinking@gmail.com>
2022-12-15 01:59:10 +0100

    jasper: build shared

--- packages/graphics/jasper/package.mk
+++ packages/graphics/jasper/package.mk
@@ -12,9 +12,10 @@ PKG_DEPENDS_TARGET="toolchain libjpeg-turbo"
 PKG_LONGDESC="A implementation of the ISO/IEC 15444-1 also known as JPEG-2000 standard for image compression."
 PKG_BUILD_FLAGS="+pic"
 
-PKG_CMAKE_OPTS_TARGET="-DJAS_ENABLE_DOC=false \
+PKG_CMAKE_OPTS_TARGET="-DJAS_ENABLE_SHARED=true \
+                       -DJAS_ENABLE_LIBHEIF=false \
+                       -DJAS_ENABLE_DOC=false \
                        -DJAS_ENABLE_PROGRAMS=false \
-                       -DJAS_ENABLE_SHARED=false \
                        -DJAS_STDC_VERSION=201710L"
 
 pre_configure_target() {
