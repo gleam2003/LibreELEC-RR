450009f6ed4d007f5e1e2990ff1996b99dcdd419
SupervisedThinking <supervisedthinking@gmail.com>
2022-12-15 02:31:13 +0100

    at-spi2-core: updated build opts for Wayland

--- packages/addons/addon-depends/chrome-depends/at-spi2-core/package.mk
+++ packages/addons/addon-depends/chrome-depends/at-spi2-core/package.mk
@@ -8,13 +8,25 @@ PKG_SHA256="aa0c86c79f7a8d67bae49a5b7a5ab08430c608cffe6e33bf47a72f41ab03c3d0"
 PKG_LICENSE="OSS"
 PKG_SITE="https://www.gnome.org/"
 PKG_URL="https://download.gnome.org/sources/at-spi2-core/${PKG_VERSION:0:4}/at-spi2-core-${PKG_VERSION}.tar.xz"
-PKG_DEPENDS_TARGET="toolchain atk dbus glib libXtst"
+PKG_DEPENDS_TARGET="toolchain atk dbus glib"
 PKG_LONGDESC="Protocol definitions and daemon for D-Bus at-spi."
 
-PKG_MESON_OPTS_TARGET="-Ddocs=false \
-                       -Dintrospection=no \
-                       -Ddbus_daemon=/usr/bin/dbus-daemon"
+configure_package() {
+  # Build with x11 support
+  if [ ${DISPLAYSERVER} = "x11" ]; then
+    PKG_DEPENDS_TARGET+=" libXtst"
+  fi
+}
 
 pre_configure_target() {
-  TARGET_LDFLAGS="${LDFLAGS} -lXext"
+  PKG_MESON_OPTS_TARGET="-Ddocs=false \
+                         -Dintrospection=no \
+                         -Ddbus_daemon=/usr/bin/dbus-daemon"
+
+  if [ ${DISPLAYSERVER} = "x11" ]; then
+    PKG_MESON_OPTS_TARGET+="-Dx11=true"
+    TARGET_LDFLAGS="${LDFLAGS} -lXext"
+  else
+    PKG_MESON_OPTS_TARGET+="-Dx11=false"
+  fi
 }
