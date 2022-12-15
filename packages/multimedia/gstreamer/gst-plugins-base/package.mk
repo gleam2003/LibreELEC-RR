1c9f5716da152f408223adc988496febe8448399
SupervisedThinking <supervisedthinking@gmail.com>
2022-12-15 02:33:29 +0100

    gst-plugins-base: fix missing libs

--- packages/multimedia/gstreamer/gst-plugins-base/package.mk
+++ packages/multimedia/gstreamer/gst-plugins-base/package.mk
@@ -12,57 +12,26 @@ PKG_LONGDESC="Base GStreamer plugins and helper libraries"
 PKG_BUILD_FLAGS="-gold"
 
 pre_configure_target() {
-  PKG_MESON_OPTS_TARGET="-Dgl=disabled \
-                         -Dadder=disabled \
-                         -Dapp=disabled \
-                         -Daudioconvert=disabled \
-                         -Daudiomixer=disabled \
-                         -Daudiorate=disabled \
-                         -Daudioresample=disabled \
-                         -Daudiotestsrc=disabled \
-                         -Dcompositor=disabled \
-                         -Dencoding=disabled \
-                         -Dgio=disabled \
-                         -Dgio-typefinder=disabled \
-                         -Doverlaycomposition=disabled \
-                         -Dpbtypes=disabled \
-                         -Dplayback=disabled \
-                         -Drawparse=enabled \
-                         -Dsubparse=enabled \
-                         -Dtcp=disabled \
-                         -Dtypefind=disabled \
-                         -Dvideoconvertscale=disabled \
-                         -Dvideorate=disabled \
-                         -Dvideotestsrc=disabled \
-                         -Dvolume=disabled \
-                         -Dalsa=disabled \
-                         -Dcdparanoia=disabled \
-                         -Dlibvisual=disabled \
-                         -Dogg=disabled \
-                         -Dopus=disabled \
-                         -Dpango=disabled \
-                         -Dtheora=disabled \
-                         -Dtremor=disabled \
-                         -Dvorbis=disabled \
-                         -Dx11=disabled \
-                         -Dxshm=disabled \
-                         -Dxi=disabled \
-                         -Dxvideo=disabled \
-                         -Dexamples=disabled \
+  PKG_MESON_OPTS_TARGET="-Dexamples=disabled \
                          -Dtests=disabled \
-                         -Dtools=disabled \
-                         -Dintrospection=disabled \
-                         -Dnls=disabled \
-                         -Dorc=disabled \
                          -Dgobject-cast-checks=disabled \
-                         -Dglib-asserts=disabled \
-                         -Dglib-checks=disabled \
                          -Dpackage-name=gst-plugins-base \
                          -Dpackage-origin=LibreELEC.tv \
-                         -Ddoc=disabled"
+                         -Ddoc=disabled \
+                         -Dnls=disabled"
+
+  # Fix undefined symbol glPointSizePointerOES
+  if [ "${OPENGLES}" = "bcm2835-driver" ]; then
+    TARGET_LDFLAGS+=" -lEGL -lGLESv2"
+  fi
+  # Fix missing dispmanx
+  if [ "${DEVICE}" = "RPi4" -o "${DEVICE}" = "RPi3" ]; then
+    PKG_MESON_OPTS_TARGET+=" -Dgl_winsys="egl""
+  fi
 }
 
 post_makeinstall_target() {
   # clean up
-  safe_remove ${INSTALL}
+  safe_remove ${INSTALL}/usr/bin
+  safe_remove ${INSTALL}/usr/share
 }
