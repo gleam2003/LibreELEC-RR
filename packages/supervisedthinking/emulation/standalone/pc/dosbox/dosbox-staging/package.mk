9361bbb7f37f737e7db31b1e781fe6076be0b503
SupervisedThinking <supervisedthinking@gmail.com>
2022-12-26 18:45:46 +0100

    Revert "dosbox-staging: update to 0.80.0"
    This reverts commit b67cbac3502bc772f0a68db97c3dd8926b0ddde4.

--- packages/supervisedthinking/emulation/standalone/pc/dosbox/dosbox-staging/package.mk
+++ packages/supervisedthinking/emulation/standalone/pc/dosbox/dosbox-staging/package.mk
@@ -2,14 +2,14 @@
 # Copyright (C) 2021-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="dosbox-staging"
-PKG_VERSION="dcb4408229992328fe1326a084ca2106469898aa" #v0.80.0
+PKG_VERSION="dd7f21809870cc998005b3b09a33f1c36bd764a6" #v0.79.1
 PKG_LICENSE="GPL-2.0-or-later"
 PKG_SITE="https://dosbox-staging.github.io/"
 PKG_URL="https://github.com/dosbox-staging/dosbox-staging.git"
 PKG_DEPENDS_TARGET="toolchain linux alsa-lib sdl2 sdl2_net opusfile libpng fluidsynth-system munt libslirp iir1 speexdsp"
 PKG_LONGDESC="DOSBox Staging is an attempt to revitalize DOSBox's development process. It's not a rewrite, but a continuation and improvement on the existing DOSBox codebase while leveraging modern development tools and practices."
 GET_HANDLER_SUPPORT="git"
-PKG_GIT_CLONE_BRANCH="release/0.80.x"
+PKG_GIT_CLONE_BRANCH="release/0.79.x"
 PKG_GIT_CLONE_SINGLE="yes"
 PKG_BUILD_FLAGS="+lto +speed"
 
